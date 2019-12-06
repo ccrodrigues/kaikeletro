67a507147feedcb31f9b73f4058faaeb7fb644f2
Luan Nascimento de Sena <luan.sena1994@gmail.com>
2019-12-06 19:27:00 -0300

    subindo a api

---
+++ agoravai/src/main/java/com/brq/ecommerce/AgoravaiApplication.java
@@ -0,0 +1,13 @@
+package com.brq.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgoravaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgoravaiApplication.class, args);
+	}
+
+}
---
+++ agoravai/src/test/java/com/brq/ecommerce/AgoravaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brq.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoravaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
