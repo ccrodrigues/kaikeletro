80122d248cef325010e17818fe23cdcfebef8b20
Kaik Xavier Marques de Oliveira <kaikoliveira@brq.com>
2019-12-06 20:19:47 -0300

    API Criada

---
+++ agoravai/src/main/java/com/brq/ecommerce/domain/Usuario.java
@@ -0,0 +1,43 @@
+package com.brq.ecommerce.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String email;
+	private String senha;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Usuario(String email, String senha) {
+		super();
+		this.email = email;
+		this.senha = senha;
+	}
+
+	public Usuario() {
+		super();
+	}
+
+}
