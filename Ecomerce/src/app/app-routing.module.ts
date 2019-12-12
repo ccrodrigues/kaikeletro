272b5cbe383de27565d4f1bf50bb4474635df8f7
Wagner Aguiar Sanches Garcia Sobrinho <wagnersanches@brq.com>
2019-12-12 19:05:07 -0300

    Wagner Rota Mundado

--- Ecomerce/src/app/app-routing.module.ts
+++ Ecomerce/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginUsuarioComponent } from './login-usuario/login-usuario.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   {path : 'login', component : LoginUsuarioComponent},
 ];
 
