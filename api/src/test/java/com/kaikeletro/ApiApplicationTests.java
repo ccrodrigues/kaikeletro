9a16f160e2c6077903e985224dfdfa1b77da6983
Wagner Sanches <wagnersanches5@hotmail.com>
2019-12-13 16:43:57 -0300

    Primeiro commit

---
+++ api/src/test/java/com/kaikeletro/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaikeletro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
