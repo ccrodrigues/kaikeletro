21f910f3c1d8d57f26cdbd65b83d5634aff6c194
gugags6 <guga.gs6@gmail.com>
2019-12-16 16:49:47 -0300

    layout alterações

---
+++ FrontEnd/src/app/home/home-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ProdutosDetalhesComponent } from './produtos-detalhes/produtos-detalhes.component';
+import { HomeComponent } from './home.component';
+
+
+const routes: Routes = [
+  {path : 'teste', component: CarouselComponent},
+  {path : 'detalhes', component: ProdutosDetalhesComponent},
+  {path : 'home', component: HomeComponent},
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
