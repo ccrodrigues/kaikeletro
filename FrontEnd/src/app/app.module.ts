c885594f34b0038e99faa00a06e62d4ff81a1a2f
Bianca <biancaqueiroz.s@hotmail.com>
2019-12-16 16:39:12 -0300

    Resolvendo conflitos e subindo footer

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardAdmComponent } from './dashboard-adm/dashboard-adm.component';
-<<<<<<< HEAD
 import { UsuarioLoginComponent } from './usuario-login/usuario-login.component';
 import { MsgErroComponent } from './usuario-login/msg-erro/msg-erro.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
@@ -12,10 +11,8 @@ import { MenuComponent } from './menu/menu.component';
 import { HomeModule } from './home/home.module';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
-import { HomeComponent } from './home/home.component';
 import { SharedModule } from './shared/shared.module';
->>>>>>> footer-viacep
+
 
 
 @NgModule({
@@ -30,15 +27,12 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     FormsModule,
     ReactiveFormsModule,
     HomeModule,
     CollapseModule.forRoot(),
-    BrowserAnimationsModule
-=======
+    BrowserAnimationsModule,
     SharedModule
->>>>>>> footer-viacep
   ],
   providers: [],
   bootstrap: [AppComponent]
