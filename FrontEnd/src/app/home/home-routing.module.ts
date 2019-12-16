c3925990de084f9c525e6da5f9d393ba8dad97ed
gugags6 <guga.gs6@gmail.com>
2019-12-16 16:43:27 -0300

    Arrumando layout do site

--- FrontEnd/src/app/home/home-routing.module.ts
+++ FrontEnd/src/app/home/home-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ProdutosDetalhesComponent } from './produtos-detalhes/produtos-detalhes.component';
+import { HomeComponent } from './home.component';
 
 
 const routes: Routes = [
   {path : 'teste', component: CarouselComponent},
-  {path : 'detalhes', component: ProdutosDetalhesComponent}
+  {path : 'detalhes', component: ProdutosDetalhesComponent},
+  {path : 'home', component: HomeComponent},
 ];
 
 @NgModule({
