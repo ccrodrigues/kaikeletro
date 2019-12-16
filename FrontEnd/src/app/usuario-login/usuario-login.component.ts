543a98cc897fcf95253cf584d014aac0cfededea
Juliana <julucacalcanti@gmail.com>
2019-12-16 14:45:47 -0300

    Subindo alterações na tela de login

--- FrontEnd/src/app/usuario-login/usuario-login.component.ts
+++ FrontEnd/src/app/usuario-login/usuario-login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, EmailValidator } from '@angular/forms';
+import { ServiceLoginService } from './service-login/service-login.service';
+
 
 @Component({
   selector: 'app-usuario-login',
@@ -9,15 +11,15 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class UsuarioLoginComponent implements OnInit {
 
   loginForm : FormGroup;
-  
 
-  constructor(private formBuilder :  FormBuilder) {
+  constructor(private formBuilder :  FormBuilder
+    , private serviceLogin : ServiceLoginService) {
 
    }
 
   ngOnInit() {
 
-
+// declaração das variaveis para a validação dos campos e-mail e senha 
     this.loginForm = this.formBuilder.group( { 
       email : [ '',[Validators.required]  ], 
       senha : [ '' , [Validators.required] ] 
@@ -26,9 +28,28 @@ export class UsuarioLoginComponent implements OnInit {
   }
   onSubmit(){
     console.log(this.loginForm);
+
+    //Verifica ao enviar se os dados informados são validos
+    this.serviceLogin
+    .fazerLogin(this.loginForm.value.email, this.loginForm.value.senha)
+
+    
+   
+  
   }
+  //campo para validar se os campos forem devidamente preenchidos 
  isErrorCampo(nomeCampo){
     return (!this.loginForm.get(nomeCampo).valid && this.loginForm.get(nomeCampo).touched ); 
+  }
+  verificarUsuario(){
+
+  }
+  //metodo para verificar se os dados de Autenticidade estão de acordo, recebe um boolean
+  isErrorLogin(){
+    return this.serviceLogin.getIsAutenticado();
+    
+    
+        
   }
  
 }
