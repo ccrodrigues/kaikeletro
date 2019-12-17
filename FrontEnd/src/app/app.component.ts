653f9c5dde40df2a9876227e0a8950ffbc5df3b3
Bianca <biancaqueiroz.s@hotmail.com>
2019-12-17 12:45:36 -0300

    Finalizada

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'FrontEnd';
   isAuth : boolean = false;
+
+  constructor(
+    private router: Router,
+  ) { }
+
+  headerFooter;
+
+  ngOnInit() {
+    this.router.events
+      .subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          this.headerFooter = (event.url !== '/login')
+        }
+      });
 }
+
+}
+
