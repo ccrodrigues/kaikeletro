2a6b499c3fe583cd5708b9c94d5197f7e92c1a12
Bianca <biancaqueiroz.s@hotmail.com>
2019-12-17 15:40:33 -0300

    Resolvendo conflitos

--- FrontEnd/src/app/usuario-login/usuario-login.component.ts
+++ FrontEnd/src/app/usuario-login/usuario-login.component.ts
@@ -32,8 +32,8 @@ export class UsuarioLoginComponent implements OnInit {
   onSubmit(){
     console.log(this.loginForm);
     //Verifica ao enviar se os dados informados são validos
-    this.serviceLogin
-    .fazerLogin(this.loginForm.value.email, this.loginForm.value.senha)
+    // this.serviceLogin
+    // .fazerLogin(this.loginForm.value.email, this.loginForm.value.senha)
   }
 
   //campo para validar se os campos forem devidamente preenchidos 
