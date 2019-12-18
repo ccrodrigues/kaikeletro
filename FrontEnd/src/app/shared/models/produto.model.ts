6892e94f3bc37c9e0888fb20ef34d220a968af86
Marcos Alves <marcosfernandesalves99@hotmail.com>
2019-12-18 11:17:01 -0300

    Adcionando o service de produtos

---
+++ FrontEnd/src/app/shared/models/produto.model.ts
@@ -0,0 +1,12 @@
+import { CategoriaModel } from './categoria.model';
+import { ImagenModel } from './imagen.model';
+
+
+export class ProdutoModel{
+    idProduto:number;
+    nome:string;
+    preco:number;
+    descricao:string;
+    categorias:Array<CategoriaModel>;
+    imagens:Array<ImagenModel>;
+}
