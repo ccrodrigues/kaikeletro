5822e87c7159ad8d750b905d3b2c6cd18ad12a7c
Bianca <biancaqueiroz.s@hotmail.com>
2019-12-19 14:07:10 -0300

    Buscar todas as vendas e salvar vendas

--- api/src/main/java/com/kaikeletro/resources/VendasController.java
+++ api/src/main/java/com/kaikeletro/resources/VendasController.java
@@ -1,10 +1,29 @@
 package com.kaikeletro.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kaikeletro.domain.Vendas;
+import com.kaikeletro.services.VendasService;
 
 @RequestMapping(value="vendas")
 @RestController
 public class VendasController {
+	
+	@Autowired
+	private VendasService service;
+	
+	
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public ResponseEntity<List<Vendas>> getAll() {
+
+		return ResponseEntity.ok().body(service.getAll());
+	}
+	
 
 }
