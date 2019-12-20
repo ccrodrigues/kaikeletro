2b9cae4ebe683f792a88ff5d9f2691e6ef5ed9bc
gugags6 <guga.gs6@gmail.com>
2019-12-20 10:06:20 -0300

    hahaha

--- api/src/main/java/com/kaikeletro/repositories/ItemVendaRepository.java
+++ api/src/main/java/com/kaikeletro/repositories/ItemVendaRepository.java
@@ -1,5 +1,11 @@
 package com.kaikeletro.repositories;
 
-public class ItemVendaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kaikeletro.domain.Item_Venda;
+
+@Repository
+public interface ItemVendaRepository extends JpaRepository <Item_Venda, Integer> {
 
 }
--- api/src/main/java/com/kaikeletro/repositories/ProdutoRepository.java
+++ api/src/main/java/com/kaikeletro/repositories/ProdutoRepository.java
@@ -3,9 +3,11 @@ package com.kaikeletro.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.kaikeletro.domain.Produto;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository <Produto, Integer>{
 	
 	//SELECT * FROM produto WHERE nome LIKE
