574164cafcfdae55f369a16c8fdd7bfa396cb190
Marcos Alves <marcosfernandesalves99@hotmail.com>
2019-12-27 10:38:13 -0300

    Arrumando a Inteceptorconfig

--- api/src/main/java/com/kaikeletro/config/ErroInterceptorHandler.java
+++ api/src/main/java/com/kaikeletro/config/ErroInterceptorHandler.java
@@ -1,10 +1,12 @@
 package com.kaikeletro.config;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
 
 import com.kaikeletro.exception.StandardError;
 
@@ -18,6 +20,34 @@ public class ErroInterceptorHandler {
 		StandardError error = new StandardError( 405,e.toString()
 				, e.getMessage() );
 		
+		return error;
+	}
+	@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+	@ExceptionHandler(BadCredentialsException.class)
+	public StandardError handle(BadCredentialsException e){
+		System.out.println(e.toString());
+		StandardError error = new StandardError( 401,e.toString()
+				, e.getMessage() );
+		
+		return error;
+	}
+	
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(BadRequest.class)
+	public StandardError handle(BadRequest e){
+		System.out.println(e.toString());
+		StandardError error = new StandardError( 400,e.toString()
+				, e.getMessage() );
+		
+		return error;
+	}
+	
+	@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(RuntimeException.class)
+	public StandardError handle(RuntimeException e){
+		System.out.println(e.toString());
+		StandardError error = new StandardError( 500,e.toString()
+				, e.getMessage() );
 		return error;
 	}
 	
