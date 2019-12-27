7503ff86a0202ccf73264bbd8a6db801afe8ed23
Romulo Zumele Martins Moraes <romulozumele@brq.com>
2019-12-27 10:48:58 -0300

    na Confirmação da venda, criação dos componentes de Confirmação de Endereço e Dados do Pagamento

---
+++ FrontEnd/src/app/vendas/pagamento-finalizacao/pagamento-finalizacao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagamento-finalizacao',
+  templateUrl: './pagamento-finalizacao.component.html',
+  styleUrls: ['./pagamento-finalizacao.component.css']
+})
+export class PagamentoFinalizacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
