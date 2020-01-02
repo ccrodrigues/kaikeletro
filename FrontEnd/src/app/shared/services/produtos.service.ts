32ccdd00b5cb8d571cb3334a4b087d659c7cc0fd
Wagner Sanches <wagnersanches5@hotmail.com>
2020-01-02 15:55:13 -0300

    SA

--- FrontEnd/src/app/shared/services/produtos.service.ts
+++ FrontEnd/src/app/shared/services/produtos.service.ts
@@ -14,6 +14,9 @@ export class ProdutosService {
   // Chamando o HttpCliente e injetando o envService que é responsavel por pegar a url Da Api
   constructor(private http : HttpClient,private envService:EnvService) { }
 
+  // Url da API
+  url : string = "http://localhost:8080/produtos"
+
   // Pegando todos os produto
   getAll() {
     //fazendo a requisição
@@ -21,23 +24,23 @@ export class ProdutosService {
   }
 
   delete(id) {
-    console.log(this.envService.urlAPI + id)
-    return this.http.delete(this.envService.urlAPI + id);
+    console.log(this.url + id)
+    return this.http.delete(this.url + id);
     // return this.http.get<ProdutoModel[]>(this.envService.urlAPI+ "produtos");
   }
 
   getProdutoDto(){
-    return this.http.get(`${this.envService.urlAPI}/produtos/carrinho`)
+    return this.http.get(`${this.envService.urlAPI}produtos/carrinho`)
   }
 
   //Busca de ProdutoDTO por id
   getProdutoDtoId(id){
-    return this.http.get<ProdutoDtoModel>(`${this.envService.urlAPI}/produtos/carrinho/${id}`)
+    return this.http.get<ProdutoDtoModel>(`${this.envService.urlAPI}produtos/carrinho/${id}`)
   }
 
   //pegando o produto filtrando por id
   getById(id){
-    return this.http.get<ProdutoModel>(this.envService.urlAPI+ "/produtos"+"/"+id);
+    return this.http.get<ProdutoModel>(this.envService.urlAPI+ "produtos"+"/"+id);
   }
   //adcionando o produto ao banco
   saveProduto(produto){
@@ -49,11 +52,11 @@ export class ProdutosService {
   }
   //editando um produto ja existente
   updateProduto(produto,id){
-    return this.http.patch<ProdutoModel>(this.envService.urlAPI+ "/produtos/"+id,produto);
+    return this.http.patch<ProdutoModel>(this.envService.urlAPI+ "/produtos"+"/"+id,produto);
   }
   //recebendo os produtos por paginacao
   getProdutoPage(pagina,qtdLinhas,direcao,campo){
-    return this.http.get<ProdutoModel[]>(this.envService.urlAPI+ "/produtos"
+    return this.http.get<ProdutoModel[]>(this.envService.urlAPI+ "produtos"
     +`pagina=${pagina}&qtdLinhas=${qtdLinhas}&direcao=${direcao}&campo=${campo}`)
   }
 }
