5bdf0da3fa54b1272827adc95f57dfba681f5364
Fabrizio Borelli <ffborelli@gmail.com>
2020-01-05 20:45:20 -0300

    adicionando busca no menu

--- FrontEnd/src/app/layout/vendas/menu/menu.component.ts
+++ FrontEnd/src/app/layout/vendas/menu/menu.component.ts
@@ -5,14 +5,10 @@ import { Router } from '@angular/router';
 import { ProdutosService } from 'src/app/shared/services/produtos.service';
 import { ProdutoModel } from 'src/app/shared/models/produto.model';
 import { Popover } from 'ngx-popover';
-import { isNull } from 'util';
 import { StorageService } from 'src/app/shared/services/storage.service';
 import { AuthServiceService } from 'src/app/shared/services/auth-service.service';
 import { DialogService } from 'src/app/shared/toaster/dialog.service';
 import { CarrinhoService } from 'src/app/shared/services/carrinho.service';
-import { ProdutosCategoriaComponent } from 'src/app/vendas/produtos-categoria/produtos-categoria.component';
-import { VendasModule } from 'src/app/vendas/vendas.module';
-
 
 @Component({
   selector: 'app-menu',
@@ -21,44 +17,43 @@ import { VendasModule } from 'src/app/vendas/vendas.module';
 })
 export class MenuComponent implements OnInit {
 
-  @ViewChild("myPopover", {static:true}) myPopover : Popover;
+  @ViewChild("myPopover", { static: true }) myPopover: Popover;
 
-   isCollapsed = false;
-   isAuth : boolean ;
-   isDashboard : boolean = false;
-   tooltipProdutos : ProdutoModel [] ;
+  isCollapsed = false;
+  isAuth: boolean;
+  isDashboard: boolean = false;
+  tooltipProdutos: ProdutoModel[];
 
-   isHiddden = true;
- 
+  isHiddden = true;
 
+  deslogar: boolean;
+  nomeCliente: string = '';
+  qntd;
 
-  constructor(private menuService : menuService, 
-    private loginService : ServiceLoginService,
-   private produtoService : ProdutosService,
-   private router: Router,
-   private storage : StorageService,
-   private authService : AuthServiceService,
-   private carrinhoService : CarrinhoService,
-   private dialogService: DialogService ) { }
-   deslogar : boolean ;
-   nomeCliente : string = '';
-   qntd;
-
+  constructor(private menuService: menuService,
+    private loginService: ServiceLoginService,
+    private produtoService: ProdutosService,
+    private router: Router,
+    private storage: StorageService,
+    private authService: AuthServiceService,
+    private carrinhoService: CarrinhoService,
+    private dialogService: DialogService
+  ) { }
 
   ngOnInit() {
 
     let localUser = this.storage.getLocalUser();
     //console.log(localUser)
-    this.nomeCliente = (localUser ? localUser.nome : '' );
+    this.nomeCliente = (localUser ? localUser.nome : '');
 
     this.isAuth = this.loginService.isAutenticado();
-    if(this.storage.getLocalUser()!=null){
+    if (this.storage.getLocalUser() != null) {
       this.isAuth = true;
 
-    }else{
+    } else {
       this.isAuth = false;
     }
-    
+
     //  this.qntd = this.carrinhoService.totalItensCarrinho();
     // // //this.isAuth = this.loginService.getIsAutenticado();
     // //     //if(this.isAuth == false){
@@ -67,40 +62,34 @@ export class MenuComponent implements OnInit {
     //  console.log(this.carrinhoService.exibirItens())
 
   }
-  
-  logadoAdmin(){
+
+  logadoAdmin() {
     this.isDashboard = this.loginService.isAdmin() && this.loginService.isAutenticado();
     return this.isDashboard;
   }
 
   //metodo que busca os itens da api de acordo com a pesquisa do usuario
-  buscarProdutos(nomeProduto){
-     console.log(nomeProduto);
-    if(nomeProduto.value != nomeProduto.isNull){
-    this.produtoService.getProdutoNome(nomeProduto.value).subscribe(
-      a => { 
-        console.log(this.myPopover);
-        this.tooltipProdutos = a;
-        //this.showPopover();
-        this.isHiddden = false;
-        
-      },
-      e => {
-        console.log(e);
-      });
+  buscarProdutos(nomeProduto) {
+    console.log(nomeProduto.value);
+    if (nomeProduto.value != "") {
+      this.produtoService.getProdutoNome(nomeProduto.value).subscribe(
+        busca => {
+          
+          this.tooltipProdutos = busca;
+          //this.showPopover();
+          this.isHiddden = false;
+        },
+        e => {
+          console.log(e);
+        });
     }
 
   }
 
-
-
-  
-
-
-  sair(){
+  sair() {
     this.deslogar = this.loginService.logout();
     this.dialogService.showSuccess('Logout feito com sucesso!');
   }
-  
+
 }
 
